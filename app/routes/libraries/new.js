b9e8f00ff9f4ac628c781d08bbe401af43ea68a5
Marshall Shen <shen.marshall@gmail.com>
2016-03-01 13:18:10 -0600

    3.2

---
+++ app/routes/libraries/new.js
@@ -0,0 +1,27 @@
+// app/routes/libraries/new.js
+
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord('library');
+  },
+
+  actions: {
+    saveLibrary(newLibrary) {
+      newLibrary.save().then(() => this.transitionTo('libraries'));
+    },
+
+    willTransition() {
+      // rollbackAttributes() removes the record from the store
+      // if the model 'isNew'
+
+      // let model = this.controller.get('model');
+      // if (model.get('isNew')) {
+      //   model.destroyRecord();
+      // }
+      this.controller.get('model').rollbackAttributes();
+    }
+  }
+
+});
