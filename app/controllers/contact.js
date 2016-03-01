b408ef1f961bf69fc5fb08c8702eed8455344991
Marshall Shen <shen.marshall@gmail.com>
2016-03-01 13:31:13 -0600

    3.3

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -3,14 +3,26 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   message: '',
 
-  isValid: Ember.computed.gte('message.length', 5),
+  isEmailValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isMessageValid: Ember.computed.gte('message.length', 5),
+  isValid: Ember.computed.and('isEmailValid', 'isMessageValid'),
   isDisabled: Ember.computed.not('isValid'),
 
   actions: {
-    sendMessage() {
-      alert(`Thank you for sending the message ${this.get('message')}`);
-      this.set('responseMessage', `Thank you for sending the message ${this.get('message')}`);
-      this.set('message', '');
+    sendMessage: function(){
+      var _that = this;
+      var email = this.get('emailAddress');
+      var message = this.get('message');
+      var newContact = this.store.createRecord('contact', {
+        email: email,
+        message: message
+      });
+
+      newContact.save().then(function(response){
+        _that.set('responseMessage', "Thank you! We saved your email address with the following id: " + response.get('id'));
+        _that.set('emailAddress', '');
+        _that.set('message', '');
+      });
     }
   }
 });
